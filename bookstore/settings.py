99b1398e3820714df107f352019d796c9e548317
oLucas-Cipriano <lucas.e.a.cipriano@gmail.com>
2024-10-15 10:28:55 -0300

    viewsets e testes para os mesmos

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
+    "rest_framework",
     "order",
     "product",
 ]
@@ -123,3 +125,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5
+
+}
