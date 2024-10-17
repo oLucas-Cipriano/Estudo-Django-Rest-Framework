4ebdaa922877054be296403ee5591d5909e530aa
oLucas-Cipriano <lucas.e.a.cipriano@gmail.com>
2024-10-17 08:50:39 -0300

    Consertando o settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -151,7 +151,3 @@ INTERNAL_IPS = [
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
-
-# 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
-# For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
