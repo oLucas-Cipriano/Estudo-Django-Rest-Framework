77c461faf457b5957ca337cfb718bacb2f5648b5
oLucas-Cipriano <lucas.e.a.cipriano@gmail.com>
2024-10-17 08:55:35 -0300

    Consertando os testes

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -148,6 +148,3 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-
-DEBUG = int(os.environ.get("DEBUG", default=0))
--- product/tests/test_viewsets/test_product_viewset.py
+++ product/tests/test_viewsets/test_product_viewset.py
@@ -1,59 +1,58 @@
 import json
 
 from django.urls import reverse
-from rest_framework.authtoken.models import Token
+from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
-from rest_framework.views import status
 
-from order.factories import UserFactory
+from order.factories import OrderFactory, UserFactory
+from order.models import Order
 from product.factories import CategoryFactory, ProductFactory
 from product.models import Product
 
 
-class TestProductViewSet(APITestCase):
+class TestOrderViewSet(APITestCase):
+
     client = APIClient()
 
     def setUp(self):
-        self.user = UserFactory()
-        token = Token.objects.create(user=self.user)
-        token.save()
-
+        self.category = CategoryFactory(title="technology")
         self.product = ProductFactory(
-            title="pro controller",
-            price=200.00,
+            title="mouse", price=100, category=[self.category]
         )
+        self.order = OrderFactory(product=[self.product])
 
-    def test_get_all_product(self):
-        token = Token.objects.get(user__username=self.user.username)
-        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
-
-        response = self.client.get(reverse("product-list", kwargs={"version": "v1"}))
+    def test_order(self):
+        response = self.client.get(
+            reverse("order-list", kwargs={"version": "v1"}))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        product_data = json.loads(response.content)
 
-        self.assertEqual(product_data["results"][0]["title"], self.product.title)
-        self.assertEqual(product_data["results"][0]["price"], self.product.price)
-        self.assertEqual(product_data["results"][0]["active"], self.product.active)
-
-    def test_create_product(self):
-        token = Token.objects.get(user__username=self.user.username)
-        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
-
-        category = CategoryFactory()
-        data = json.dumps(
-            {"title": "notebook", "price": 800.00, "categories_id": [category.id]}
+        order_data = json.loads(response.content)
+        self.assertEqual(
+            order_data["results"][0]["product"][0]["title"], self.product.title
+        )
+        self.assertEqual(
+            order_data["results"][0]["product"][0]["price"], self.product.price
+        )
+        self.assertEqual(
+            order_data["results"][0]["product"][0]["active"], self.product.active
+        )
+        self.assertEqual(
+            order_data["results"][0]["product"][0]["category"][0]["title"],
+            self.category.title,
         )
 
+    def test_create_order(self):
+        user = UserFactory()
+        product = ProductFactory()
+        data = json.dumps({"products_id": [product.id], "user": user.id})
+
         response = self.client.post(
-            reverse("product-list", kwargs={"version": "v1"}),
+            reverse("order-list", kwargs={"version": "v1"}),
             data=data,
             content_type="application/json",
         )
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        created_product = Product.objects.get(title="notebook")
-
-        self.assertEqual(created_product.title, "notebook")
-        self.assertEqual(created_product.price, 800.00)
+        created_order = Order.objects.get(user=user)
